340e754169c008c55fdccdda24c6e3d5b779a1bc
Luciano <lucianosccp2024@gmail.com>
2026-05-25 09:38:14 -0300

    Adicionando "toString" na classe Endereco e trantando alguns erros

--- untitled/src/br/com/consultaceps/BuscandoCep.java
+++ untitled/src/br/com/consultaceps/BuscandoCep.java
@@ -11,27 +11,30 @@ import java.net.http.HttpResponse;
 public class BuscandoCep {
 
     public Endereco buscaEndereco(String cep) throws IOException, InterruptedException {
-        URI endereco = URI.create("https://viacep.com.br/ws/"+ cep + "/json/");
-        try {
-            HttpClient client = HttpClient.newHttpClient();
+        URI endereco = URI.create("https://viacep.com.br/ws/"+ cep + "/json/"); //API USADA
+        try { //TENTE RODAR ISSO
+            HttpClient client = HttpClient.newHttpClient(); //CRIA UM HTTP
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(endereco)
                     .build();
             HttpResponse<String> response = client
                     .send(request, HttpResponse.BodyHandlers.ofString());
 
-            if (response.statusCode() == 200) {
-                System.out.println("Pesquisa realizada com sucesso!");
-
-            } else if(response.body().contentEquals("\"erro\"")) {
+            if(response.body().contains("\"erro\"")) {
                 throw new Exception("Erro ao pesquisar CEP!");
+            }else {
+                System.out.println("Pesquisa relizada! Confira o resultado:");
             }
+            //ESSE BLOCO DE CODIGO ↑ ENTREGA A RESPOSTA DA PESQUISA EM JSON
 
+            //E ESSE RETURN ↓ CONVERTE O JSON EM OBJETO JAVA USANDO GSON PRA FICAR "BONITINHO"
             return new Gson().fromJson(response.body(),Endereco.class);
+            //Lê: "RETORNA UM NOVO GSON CONVERTENDO O CORPO DA RESPOSTA
+            //DE ACORDO COM A CLASSE RECORD Endereco"
 
         } catch (Exception e) {
-            throw new RuntimeException("Não foi possível concluir a busca," +
-                    "Por favor reveja os dados inseridos.");
+            throw new RuntimeException("\nNão foi possível concluir a busca," +
+                    " por favor reveja os dados inseridos.");
         }
     }
 
--- untitled/src/br/com/consultaceps/Endereco.java
+++ untitled/src/br/com/consultaceps/Endereco.java
@@ -1,4 +1,15 @@
 package br.com.consultaceps;
 
 public record Endereco(String cep, String localidade, String uf) {
+
+    @Override
+    public String toString() {
+        return """
+            CEP : %s
+            LOCALIDADE : %s
+            ESTADO : %s
+            """.formatted(cep, localidade, uf);
+    }
+
+
 }
